5011d7a22b91248acfe888c83059872ddb7ad8c2
Chaos <Chaos1728@outlook.com>
2023-06-18 11:36:44 +0800

    定期提交：完成CNN的方位估计模型的编写，开始着手模型训练

--- datasetGenerate.py
+++ datasetGenerate.py
@@ -11,6 +11,7 @@ from scipy.signal import hilbert
 import pickle
 import os
 import time
+import torch
 
 
 def signalGenerate(theta0, sampleTime=10, eleNum=10, eleSpacing=1.5,
@@ -97,8 +98,47 @@ def sigCW(theta0, sampleTime=2, eleSpacing=1.5, eleNum=10, sampleFreq=51200, fre
     return arraySignal, tScale
 
 
-def dataGenerator():
-    pass
+def dataSplit(fpath):
+    """加载同时预处理数据集，只针对特定数据生效，并且做一个分割，分割成训练集，验证集，测试集并保存"""
+    with open(fpath, 'rb') as f:
+        data = pickle.load(f)
+
+    # ==================================== 数据集预处理 ====================================
+    dataset = [None] * len(data)
+    tmpData = np.zeros((2, 10, 10), dtype=float)
+    thetaList = list(range(-90, 91, 1))
+    for i in range(len(data)):
+        tmpData[0] = data[i][0].real
+        tmpData[1] = data[i][0].imag
+        index = thetaList.index(data[i][1])
+        tmpBearing = np.zeros((1, 181), dtype=int)
+        tmpBearing[:, index] = 1
+        dataset[i] = (torch.Tensor(tmpData), torch.Tensor(tmpBearing))
+
+    training = [None] * 101903
+    validate = 33847 * [None]
+    test = 45250 * [None]
+    for i in range(181):
+        index = i * 1000
+        training[563 * i:563 * (i + 1)] = dataset[index:index + 563]
+        validate[187*i:187*(i+1)] = dataset[index+563:index + 750]
+        test[250*i:250*(i+1)] = dataset[index+750:index+1000]
+
+    # 保存数据集
+    (dataPath, dataName) = os.path.split(fpath)
+    subFolderName = dataName.split(".")[0]
+    folderPath = os.path.join(dataPath, subFolderName)
+    if not os.path.isdir(folderPath):
+        os.makedirs(folderPath)
+
+    with open(os.path.join(folderPath, "trainLoader.pkl"), 'wb') as f:
+        pickle.dump(training, f)
+    with open(os.path.join(folderPath, "validation.pkl"), 'wb') as f:
+        pickle.dump(validate, f)
+    with open(os.path.join(folderPath, "testLoader.pkl"), 'wb') as f:
+        pickle.dump(test, f)
+
+    return training, validate, test
 
 
 if __name__ == '__main__':
--- modelTraining.py
+++ modelTraining.py
@@ -6,30 +6,125 @@
     @Date: 2023/6/8
     @Description: 
 """
-import os, pickle
+import time
+import os
+import pickle
+import numpy as np
+from scipy.io import savemat
+import torch.cuda
 import torch.nn as nn
-import torch.nn.functional as F
+from datasetGenerate import dataSplit
 
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()  # 创建基类 nn.Module 的 1 个实例
         # 根据架构图创建全连接层
-        self.fc1 = nn.Linear(28 * 28, 200)
-        self.fc2 = nn.Linear(200, 200)
-        self.fc3 = nn.Linear(200, 10)
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=2, out_channels=32, kernel_size=2, stride=1, padding='same'),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=2, stride=1, padding='same'),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=1)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding='same'),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=3)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(in_features=576, out_features=1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_features=1024, out_features=512),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_features=512, out_features=181)
+        )
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = x.view(-1, 576)  # 返回新张量，数据相同，形状不同
+        x = self.fc(x)
+        return x
 
 
 if __name__ == '__main__':
     print("================== modelTraining ==================")
+    print("Start Time: " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 
-    # 导入数据
-    datasetPath = ".\\dataset"
-    datasetFilename = "M10SNR20.pkl"
-    dataset = os.path.join(datasetPath, datasetFilename)
-    with open(dataset, 'rb') as f:
-        data = pickle.load(f)
+    # ==================================== 导入数据集 ====================================
+    filepath = ".\\dataset"
+    filename = "M10SNR20.pkl"
 
-    # 搭建模型
+    subFolderPath = os.path.join(filepath, filename.split(".")[0])
+
+    # 第一次运行的时候分割数据集，往后直接调用
+    if not os.path.isdir(subFolderPath):
+        fileFullPath = os.path.join(filepath, filename)
+        trainLoader, validation, _ = dataSplit(fileFullPath)
+    else:
+        files = os.listdir(subFolderPath)
+        with open(os.path.join(subFolderPath, "trainLoader.pkl"), 'rb') as f:
+            trainLoader = pickle.load(f)
+        with open(os.path.join(subFolderPath, "validation.pkl"), 'rb') as f:
+            validation = pickle.load(f)
+
+    trainingDataLoader = torch.utils.data.DataLoader(trainLoader, batch_size=16, shuffle=True)
+
+    # 初始化模型并转移到GPU上
+    netModel = Net()
+    if torch.cuda.is_available():
+        netModel.cuda()
+
+    # ==================================== 训练模型 ====================================
+    # 定义损失函数（loss criterion）
+    criterion = nn.CrossEntropyLoss()
+    # 创建随机梯度下降优化器（optimizer）
+    optimizer = torch.optim.SGD(netModel.parameters(), lr=0.001, momentum=0.9)
+
+    print("Training Start: " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    print("Model Training...")
+    epochs = 50
+    runningLossList = []
+    lossList = []
+    for epoch in range(epochs):
+        runningLoss = 0.0
+        for i, (inputs, bearingLabels) in enumerate(trainingDataLoader, 0):
+            if torch.cuda.is_available():
+                inputs, bearingLabels = inputs.cuda(), bearingLabels.cuda()
+            optimizer.zero_grad()
+            outputBearing = netModel(inputs)
+            bearingLabels = torch.squeeze(bearingLabels, 1)  # 降维，可删除
+            loss = criterion(outputBearing, bearingLabels)
+            loss.backward()
+            optimizer.step()
+            runningLoss += loss.item()
+        runningLossList.append(runningLoss / (i + 1))
+        lossList.append(loss.data.tolist())
+        print('Epoch %d, Running Loss: %.3f, Loss = %f.' % (epoch + 1, runningLoss / (i + 1), loss.data.tolist()))
+
+    TrainingEndedTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    print("Training Ended: " + TrainingEndedTime)
+
+    # ==================================== 测试模型 ====================================
+    # netModel = torch.load(".\\model\\netmodel.pth")
+
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for (inputs, bearingLabels) in validation:
+            if torch.cuda.is_available():
+                inputs, bearingLabels = inputs.cuda(), bearingLabels.cuda()
+            outputBearing = netModel(inputs)
+            _, predicted = torch.max(outputBearing.data, dim=1)
+            total += bearingLabels.size(0)
+            correct += (predicted == bearingLabels).sum().item()
+    print('Accuracy of the network on the validation set: %d %%' % (100 * correct / total))
+
+    # ==================================== 保存参数 ====================================
+    savedName = ".\\model\\netmodel" + TrainingEndedTime + ".pth"
+    torch.save(netModel, savedName)
+    savedParam = ".\\model\\netmodelParam" + TrainingEndedTime + ".mat"
+    savemat(savedParam, {"runningLossList":runningLoss, "lossList":lossList, "correctRate": (100 * correct / total)})
 
     print("================== プログラム終了 ==================")
